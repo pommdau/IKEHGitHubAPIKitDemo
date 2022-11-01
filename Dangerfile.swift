f9061cad46f9774ece3f7b4a0c5d092b44c13de2
IKEH1024 <pondau1024@gmail.com>
2022-11-01 22:51:52 +0900

    initial commit

---
+++ Dangerfile.swift
@@ -0,0 +1,8 @@
+import Danger 
+let danger = Danger()
+
+let pr = danger.github.pullRequest
+let changedLinesCount = (pr.additions ?? 0) + (pr.deletions ?? 0)
+if changedLinesCount > 300 {
+    danger.warn("変更量が300行を超えています。PRを分割してください。")
+}
---
+++ IKEHGitHubAPIKitDemo/IKEHGitHubAPIKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IKEHGitHubAPIKitDemoApp.swift
+//  IKEHGitHubAPIKitDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/11/01.
+//
+
+import SwiftUI
+
+@main
+struct IKEHGitHubAPIKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
