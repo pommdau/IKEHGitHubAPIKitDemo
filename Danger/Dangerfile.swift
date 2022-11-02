dd80d9145f9e74e608a0f2eef3988a223079b6bd
IKEH1024 <pondau1024@gmail.com>
2022-11-02 09:08:07 +0900

    update danger

--- Danger/Dangerfile.swift
+++ Danger/Dangerfile.swift
@@ -4,5 +4,5 @@ let danger = Danger()
 let pr = danger.github.pullRequest
 let changedLinesCount = (pr.additions ?? 0) + (pr.deletions ?? 0)
 if changedLinesCount > 300 {
-    danger.warn("変更量が300行を超えていますよーんよーん。PRを分割してください。")
+    danger.warn("変更量が300行を超えています🐱。PRを分割してください。")
 }
